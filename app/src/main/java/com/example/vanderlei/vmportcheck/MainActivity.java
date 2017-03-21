8be602ff829ee6a9b2fdc1e8a663c75404e65fc5
hdelei <hdelei@gmail.com>
2017-03-20 21:31:14 -0300

    No modifications

--- app/src/main/java/com/example/vanderlei/vmportcheck/MainActivity.java
+++ app/src/main/java/com/example/vanderlei/vmportcheck/MainActivity.java
@@ -3,10 +3,13 @@ package com.example.vanderlei.vmportcheck;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.text.InputType;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -14,6 +17,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         final Button btCheck = (Button) findViewById(R.id.btCheck);
         btCheck.setFocusable(true);
