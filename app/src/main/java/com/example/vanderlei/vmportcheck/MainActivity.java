98efdca50dab85b744ff15998e1f8ac8e24348b2
hdelei <hdelei@gmail.com>
2017-03-20 21:54:39 -0300

    No modifications

--- app/src/main/java/com/example/vanderlei/vmportcheck/MainActivity.java
+++ app/src/main/java/com/example/vanderlei/vmportcheck/MainActivity.java
@@ -3,13 +3,10 @@ package com.example.vanderlei.vmportcheck;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.res.Configuration;
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.text.InputType;
-import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -17,8 +14,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
