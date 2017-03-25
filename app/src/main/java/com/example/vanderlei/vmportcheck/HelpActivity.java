0dff4254bd19759d5c9efa27c29e2ecb81ede44c
hdelei <hdelei@gmail.com>
2017-03-24 23:49:17 -0300

    Inserted Help/About html document

--- app/src/main/java/com/example/vanderlei/vmportcheck/HelpActivity.java
+++ app/src/main/java/com/example/vanderlei/vmportcheck/HelpActivity.java
@@ -2,6 +2,8 @@ package com.example.vanderlei.vmportcheck;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 import android.widget.TextView;
 
 public class HelpActivity extends AppCompatActivity {
@@ -12,6 +14,11 @@ public class HelpActivity extends AppCompatActivity {
         setContentView(R.layout.activity_help);
 
         TextView text = (TextView) findViewById(R.id.lblStatus);
+
+        WebView helpView = (WebView) findViewById(R.id.helpV);
+        WebSettings settings = helpView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        helpView.loadUrl("file:///android_asset/help_about_apk.html");
 
 
     }
